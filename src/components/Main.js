f1134b01de1806705bce5d1cda483330a16556b4
kulamce <kulamce@gmail.com>
2021-04-24 23:17:41 +0200

    witamy w 2k21

--- src/components/Main.js
+++ src/components/Main.js
@@ -92,13 +92,7 @@ class Main extends React.Component {
             <img src={pic03} alt="" />
           </span>
           <p>
-            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
-            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
-            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
-            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
-            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
-            amet.
+            Czy cie nie pojebalo przypadkiem mordko?
           </p>
           {close}
         </article>
