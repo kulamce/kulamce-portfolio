8c3626f1549c3e9450bc7b85567f983a084ce18e
kulamce <kulamce@gmail.com>
2021-04-26 15:02:59 +0200

    aaa

--- src/components/Main.js
+++ src/components/Main.js
@@ -37,17 +37,26 @@ class Main extends React.Component {
             WITAM KURWA JEGO MAC!
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-            libero eu nibh porttitor fermentum. Nullam venenatis erat id
-            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
+             <ul className="icons">
+            <li>
+              <a className="icon fab fa-sass">
+                <span className="label">SASS</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://instagram.com/cezvrx" className="icon fa-instagram">
+                <span className="label">Instagram</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/kulamce/kulamce-portfolio"
+                className="icon fa-github"
+              >
+                <span className="label">GitHub</span>
+              </a>
+            </li>
+          </ul>
           </p>
           {close}
         </article>
